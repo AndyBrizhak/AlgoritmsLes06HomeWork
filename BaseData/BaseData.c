2bd69b50dc02a92ecbe1eb82b21078f175fdc307
AndyBrizhak <andrewbrizhak@gmail.com>
2018-05-03 09:13:37 +0300

    + добавлен алгоритм вставку узлов студентов в бинарное дерево

--- BaseData/BaseData.c
+++ BaseData/BaseData.c
@@ -11,6 +11,7 @@ typedef struct student {
 	
 	int weight[4]; 
 	} Student;
+
 typedef Student T;
 
 typedef struct Node {
@@ -20,11 +21,81 @@ typedef struct Node {
 	struct Node * parent;
 } Node;
 
+// Создание нового узла
+Node * getFreeNode(T current, Node * parent) {
+	Node * tmp = (Node *)malloc(sizeof(Node));
+	tmp->left = tmp->right = NULL;
+	tmp->data = current;
+	tmp->parent = parent;
+	return tmp;
+}
+
+// Вставка узла
+void insert(Node ** head, Student current) {
+	Student * tmpStudent = (Student *)malloc(sizeof(Student));
+	Node * tmp = NULL;
+	if (*head == NULL)
+	{
+		*head = getFreeNode(current, NULL);
+		return;
+	}
+	tmp = *head;
+	while (tmp)
+	{
+		Student tmpStudent = tmp->data;
+		/*int tmpNumber = tmpStudent->number;*/
+		if (current.number > tmpStudent.number)
+		{
+			if (tmp->right)
+			{
+				tmp = tmp->right;
+				continue;
+			}
+			else
+			{
+				tmp->right = getFreeNode(current, tmp);
+				return;
+			}
+		}
+		else if (current.number < tmpStudent.number)
+		{
+			if (tmp->left)
+			{
+				tmp = tmp->left;
+				continue;
+			}
+			else
+			{
+				tmp->left = getFreeNode(current, tmp);
+				return;
+			}
+		}
+		else
+		{
+			exit(2); // дерево построено неправильно
+		}
+	}
+}
+
+void preOrderTravers(Node * root) {
+	if (root) {
+		printf("%d ", root->data);
+		preOrderTravers(root->left);
+		preOrderTravers(root->right);
+	}
+}
 
 
 int main()
 {
-	Student [4] = {{ 1234, "Vasia", 20, 78},{ 1235, "Masha", 19, 50 },{ 1230, "Grusha", 18, 52 },{ 1220, "Perikl", 22, 78 }};
+	Student list [4] = {{ 1234, "Vasia", 20, 78},{ 1235, "Masha", 19, 50 },{ 1230, "Grusha", 18, 52 },{ 1220, "Perikl", 22, 78 }};
+	Node * Tree = NULL;
+	int i;
+	for (i = 0; i < 4; i++)
+	{
+	
+		insert(&Tree, list[i]);
+	}
 	 
 	return 0;
 }
